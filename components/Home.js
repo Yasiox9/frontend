030dde93c2e1a3d4884aad426703a48ca64ef3db
Yacine <Yacine.lariane@gmail.com>
2023-07-05 14:54:19 +0200

    frontend

--- components/Home.js
+++ components/Home.js
@@ -12,7 +12,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/movies")
+    fetch("https://mymoviesback-nine.vercel.app/")
       .then((response) => response.json())
       .then((data) => {
         setMoviesData(data.movies.results);
